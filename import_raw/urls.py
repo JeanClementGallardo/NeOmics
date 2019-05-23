abe83bfd1e8ff45569328954bcb66ab4759416a4
EliotRagueneau <eliot.ragueneau@etu.u-bordeaux.fr>
2019-05-23 16:18:39 +0200

    Correct settings

--- import_raw/urls.py
+++ import_raw/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 
-app_name = "compute_graph"
+app_name = "import_raw"
 urlpatterns = [
     path('', views.index, name='index'),
     path('name/', views.get_name, name="name"),
