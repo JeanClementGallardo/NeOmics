3ec779cd03ef03126e1a3d2716395549bb9b01e4
jean_clement <jean-clement.gallardo@etu.u-bordeaux.fr>
2019-05-23 14:04:10 +0200

    Add models in import raw

--- import_raw/models.py
+++ import_raw/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 # Create your models here.
-class Graph(models.Model):
+class Labkey(models.Model):
     uri = models.CharField(max_length=200)
+    users = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+
+
+class Path(models.Model):
+    filepath = models.FilePathField(max_length=500)
+
 
