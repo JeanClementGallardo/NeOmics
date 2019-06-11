aeb58fe94440b808bb681a301aca6510b9ae6145
JeanClementGallardo <jean-clement.gallardo@etu.u-bordeaux.fr>
2019-06-11 15:15:49 +0200

    continue viewer + css

--- Viewer/static/Viewer/QueryMaker.js
+++ Viewer/static/Viewer/QueryMaker.js
@@ -54,9 +54,10 @@ function update_global_query() {
     let returned = " RETURN ";
     let return_sth = false;
     for (let node of nodes) {
-
+        node.get_types();
         matches += "(";
-        if (node.returned === 1) {
+        node.returned = node.check_return.is(':checked');
+        if (node.returned) {
             let node_id = ids.next().value;
             matches += node_id;
             returned += `${node_id}, `;
@@ -75,16 +76,15 @@ function update_global_query() {
             link.update_type_list();
             matches += "-[";
             if (link.simple) {
-                // link.type = link.select_type.find(":selected").val();
-                // console.log(link.select_type.find(":selected").val());
                 let link_type = link.type;
+                link.returned = link.check_return.is(':checked');
                 if (link.returned) {
-                    let link_id = next(ids);
+                    let link_id = ids.next().value;
                     matches += link_id;
                     returned += `${link_id}, `;
                     return_sth = true;
                 }
-                if (link_type) {
+                if (link_type && link.type !== "Unknown") {
                     matches += `:${link_type}`;
                 }
             } else {
@@ -92,12 +92,12 @@ function update_global_query() {
             }
             matches += "]-";
         }
-        if (return_sth) {
-            returned.substring(0, (returned.length - 2));
-        }
-        query = matches + returned;
-        $("#cypher").val(query);
     }
+    if (return_sth) {
+        returned = returned.substring(0, (returned.length - 2));
+    }
+    query = matches + returned;
+    $("#cypher").val(query);
 }
 
 let node_id_generator = id_generator();
@@ -113,17 +113,12 @@ class Node {
         this.name = "";
         this.link = null;
 
-        this.node_div = $(`<div id=${this.id}></div>`);
+        this.node_div = $(`<div id=${this.id} class="selectBox"></div>`);
         this.select_type = $(`<SELECT onchange="update_global_query()"></SELECT>`);
-        this.check_return = $(`<input type="checkbox" onchange="return_value()">Return</input>`);
-        this.get_types();
+        this.check_return = $(`<input type="checkbox" onchange="update_global_query()" id=${this.id}>Return</input>`);
         this.node_div.append(this.select_type);
         this.node_div.append(this.check_return);
         add_btn.before(this.node_div);
-    }
-
-    return_value(){
-        this.returned = !this.returned;
     }
 
     get descriptor() {
@@ -146,13 +141,13 @@ class Node {
             cypher += node.descriptor;
 
             let link = node.link;
+            console.log(link);
             if (link) {
-                link.update_type_list();
                 cypher += "-[";
                 if (link.simple) {
                     let link_type = link.type;
-                    if (link_type) {
-                        cypher += `:"${link_type}"`;
+                    if (link_type && link_type !== "Unknown") {
+                        cypher += `:${link_type}`;
                     }
                 } else {
                     cypher += `*${link.min}..${link.max}`;
@@ -160,55 +155,49 @@ class Node {
                 cypher += "]-";
             }
         }
+        console.log(cypher);
         return cypher;
     }
 
-    set_type() {
-        this.type = this.select_type.find(":selected").text();
+    remove() {
+        this.node_div.remove();
     }
 
     get_types() {
+        this.type = this.select_type.find(":selected").val();
+        let selected = this.select_type.find(":selected").val();
         let types_query = this.query + "(a) RETURN DISTINCT labels(a) as type";
+        this.select_type.empty();
+        this.select_type.append($("<OPTION value='Unknown'>Unknown</OPTION>"));
         session.run(types_query).then(result => {
-            this.select_type.empty();
-            this.select_type.append($("<OPTION value='Unknown'>Unknown</OPTION>"));
+            console.log(result);
             for (let record of result.records) {
                 let label = record.get('type')[0];
-                this.select_type.append($(`<OPTION value='${label}'>${label}</OPTION>`));
+                console.log(label);
+                if (label === selected) {
+                    this.select_type.append($(`<OPTION value='${label}' selected>${label}</OPTION>`));
+                } else {
+                    this.select_type.append($(`<OPTION value='${label}'>${label}</OPTION>`));
+                }
             }
         });
     }
-
-
-    // update_name_list() {
-    //     let name_query = this.query + '(a';
-    //     if (this.type) {
-    //         name_query += `:${this.type}`;
-    //     }
-    //     name_query += ') RETURN a.name';
-    //     // completion list is a set to avoid repetition
-    //     let results = session.run(name_query);
-    //     for (name of results) {
-    //         //    update name selection
-    //     }
-    // }
 }
+
 
 function new_node() {
     let last_node = nodes[nodes.length - 1];
     last_node.link = new Relation(last_node);
     last_node.link.set_next();
-    // last_node.link.next.update_name_list();
     update_global_query();
 }
 
 function remove_node() {
-    let node_id = nodes[nodes.length -1].id;
-    let rel_id = "Rel" + nodes[nodes.length -2].id.substr(node_id.length - 1);
-    document.getElementById(node_id).parentNode.removeChild(document.getElementById(node_id));
-    document.getElementById(rel_id).parentNode.removeChild(document.getElementById(rel_id));
+    nodes.pop().remove();
+    nodes[nodes.length - 1].link.remove();
     update_global_query();
 }
+
 
 class Relation {
 
@@ -221,18 +210,20 @@ class Relation {
         this.max = 0;
 
         this.previous = previous;
-        this.rel_div = $(`<div id=${this.id}></div>`);
+        this.rel_div = $(`<div id=${this.id} class="selectBox"></div>`);
+        this.image = $(`<img src="${simpleLinkPath}" onclick="Relation.dist_switch(${this.previous.id})">`);
 
+        this.rel_div.append(this.image);
         this.select_type = $(`<SELECT onchange="update_global_query()"></SELECT>`);
+        this.check_return = $(`<input type="checkbox" onchange="update_global_query()" id=${this.id}>Return</input>`);
         this.rel_div.append(this.select_type);
+        this.rel_div.append(this.check_return);
         add_btn.before(this.rel_div);
 
     }
 
     set_next() {
         this.next = new Node();
-        this.next.select_type.change(this.update_type_list);
-        this.update_type_list();
     }
 
     update_type_list() {
@@ -240,6 +231,7 @@ class Relation {
         let selected = this.select_type.find(":selected").val();
         let type_query = this.previous.query + this.previous.descriptor + `-[r]-() RETURN DISTINCT type(r) as types`;
         this.select_type.empty();
+        this.select_type.append($("<OPTION value='Unknown'>Unknown</OPTION>"));
         session.run(type_query).then(result => {
             for (let record of result.records) {
                 let label = record.get('types');
@@ -252,9 +244,20 @@ class Relation {
         });
     }
 
-    dist_switch() {
-        this.simple = !this.simple;
-        update_global_query();
+    remove() {
+        this.rel_div.remove();
+        this.previous.link = null;
+    }
+
+
+    static dist_switch(id) {
+        console.log(id);
+        let link = nodes.find(node => node.id === id.id).link;
+
+        link.simple = !link.simple;
+        link.image.attr("src",composedLinkPath);
+
+        // update_global_query();
     }
 
     update_max() {
